11d471d0b0a776fd7841b121881b6ae413d1f290
LeBaoChi <lebaochi1809@gmail.com>
2017-04-22 23:37:30 +0700

    GoogleMapPlacr

---
+++ app/src/main/java/com/example/lbochi/googlemapplace/POJO/Geometry.java
@@ -0,0 +1,34 @@
+package com.example.lbochi.googlemapplace.POJO;
+
+/**
+ * Created by Lê Bảo Chi on 4/22/2017.
+ */
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Geometry {
+
+    @SerializedName("location")
+    @Expose
+    private Location location;
+
+    /**
+     *
+     * @return
+     * The location
+     */
+    public Location getLocation() {
+        return location;
+    }
+
+    /**
+     *
+     * @param location
+     * The location
+     */
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+}
